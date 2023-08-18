edc100dc54c32bbf288faa15ba7506587c17671a
Maciej Nowak <maciejt.nowak@gmail.com>
2023-08-18 18:51:07 +0200

    Allow per-application tuning of FreeRTOS configuration

--- demo/libs/external/free_rtos/src/static.cpp
+++ demo/libs/external/free_rtos/src/static.cpp
@@ -9,3 +9,15 @@ extern "C" void vApplicationGetIdleTaskMemory(StaticTask_t** ppxIdleTaskTCBBuffe
     *ppxIdleTaskStackBuffer = &xIdleStack[0];
     *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
 }
+
+#if configUSE_TIMERS == 1
+extern "C" void vApplicationGetTimerTaskMemory(StaticTask_t** ppxTimerTaskTCBBuffer, StackType_t** ppxTimerTaskStackBuffer, uint32_t* pulTimerTaskStackSize)
+{
+    static StaticTask_t xTimerTaskTCBBuffer;
+    static StackType_t xTimerStack[configTIMER_TASK_STACK_DEPTH];
+
+    *ppxTimerTaskTCBBuffer = &xTimerTaskTCBBuffer;
+    *ppxTimerTaskStackBuffer = &xTimerStack[0];
+    *pulTimerTaskStackSize = configTIMER_TASK_STACK_DEPTH;
+}
+#endif
