619d10e97ba96155b722a30ebece1581689d75b6
Maciej Nowak <maciejt.nowak@gmail.com>
2023-09-14 18:22:21 +0200

    Tests with traces in directories

--- tool/src/tests/test_compare_test_output.py
+++ tool/src/tests/test_compare_test_output.py
@@ -25,7 +25,9 @@ def generate_packet_dump_from_packet_dump(lines: Iterable[str], output: IO[str])
     generate_packet_dump(trace_messages, output)
 
 
-@pytest.mark.parametrize('trace_name', ['task_notify'])
+TRACES = [p.name for p in Path(__file__).parent.glob('traces/*') if p.is_dir()]
+
+@pytest.mark.parametrize('trace_name', TRACES)
 def test_generate_expected_trace_output(trace_name: str) -> None:
     raw_trace_path = Path(__file__).parent / 'traces' / trace_name / 'raw.trace'
     expected_output_path = Path(__file__).parent / 'traces' / trace_name / 'output.txt'
@@ -36,34 +38,3 @@ def test_generate_expected_trace_output(trace_name: str) -> None:
         generate_output(raw_trace, output)
 
     assert output.getvalue() == expected_output_path.read_text()
-
-
-@pytest.mark.parametrize('trace_name', ['task_notify'])
-def test_generate_expected_packet_dump_from_trace_file(trace_name: str) -> None:
-    raw_trace_path = Path(__file__).parent / 'traces' / trace_name / 'raw.trace'
-    expected_output_path = Path(__file__).parent / 'traces' / trace_name / 'packet_dump.txt'
-
-    output = StringIO()
-
-    with raw_trace_path.open('rb') as raw_trace:
-        generate_packet_dump_from_trace_file(raw_trace, output)
-
-    output_s = output.getvalue()
-    expected_output_s = expected_output_path.read_text()
-    assert len(output_s) == len(expected_output_s)
-    assert output_s == expected_output_s
-
-
-@pytest.mark.parametrize('trace_name', ['task_notify'])
-def test_generate_expected_packet_dump_from_packet_dump(trace_name: str) -> None:
-    packet_dump_path = Path(__file__).parent / 'traces' / trace_name / 'packet_dump.txt'
-
-    output = StringIO()
-
-    with packet_dump_path.open('r') as packet_dump:
-        generate_packet_dump_from_packet_dump(packet_dump, output)
-
-    output_s = output.getvalue()
-    expected_output_s = packet_dump_path.read_text()
-    assert len(output_s) == len(expected_output_s)
-    assert output_s == expected_output_s
